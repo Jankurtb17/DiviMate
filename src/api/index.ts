88c8e09de7b9a104dfeb9a31cbfdbe0544aaf7d6
Jan Kurt Bayaras <j.kurtbayaras@gmail.com>
2025-03-17 18:45:54 +0800

    refactor: fix console error

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import { api } from '../boot/axios'
-import type { UserRegistration, VerifyUser } from './models/user';
+import type { UserRegistration, VerifyUser, UserLoginRequest } from './models/user';
 
 export const googleSign = async (google_token: string) => {
   const response = await api.post('/auth/google', {
@@ -18,9 +18,26 @@ export const userRegistration = async(registration: UserRegistration) => {
   }
 }
 
-export const verifyUser = async(verify_request: VerifyUser) => {
+export const verifyUser = async(verifyRequest: VerifyUser) => {
   try {
-    const response = await api.post('/v1/users/verify', verify_request)
+    const response = await api.post('/v1/users/verify', verifyRequest)
+    return response
+  } catch (error) {
+    throw error
+  }
+}
+
+export const loginUser = async (loginRequest: UserLoginRequest) => {
+  try  {
+    const formData = new URLSearchParams();
+    formData.append('username', loginRequest.username);
+    formData.append('password', loginRequest.password);
+
+    const response = await api.post('/auth/token', formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
     return response
   } catch (error) {
     throw error
